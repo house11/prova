5cac2ea0fae29aa0634a6400efa30b7b4612f856
Francesco M <francesco.mizzi@libero.it>
2020-04-02 09:54:02 +0200

    ok

--- Description.cs
+++ Description.cs
@@ -3,7 +3,7 @@ namespace Prodotti_grafica
     public class Description
     {
         public int id { get; set; > // id
-        public int IdProducts { get; set; }
+        public int IdProducts { get; set; }//34
         public string Name { get; set; }
 
         public string Info { get; set; }
